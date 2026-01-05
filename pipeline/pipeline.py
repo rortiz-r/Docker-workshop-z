f1bd0d1d2790fa41d7db7cd65397f001dde387fa
Rafael <rafael@ssh.com>
2026-01-05 15:44:57 -0500

    Pipeline

---
+++ pipeline/pipeline.py
@@ -0,0 +1,21 @@
+import sys
+import pandas as pd
+
+
+month = int(sys.argv[1])
+
+
+if month in range(1, 12):
+    print(f"Month is {month}")
+else:
+    print("Incorrect month, should be from 1 to 12")
+
+
+df = pd.DataFrame({'Day':  [1,2], 'Passengers': [3,4]})
+df['month'] = month
+
+print(df.head())
+
+df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
+
+
